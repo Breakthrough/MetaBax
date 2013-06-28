1e3a3e6fd62df3c4298b7f1385940dbe8c914852
Breakthrough <brandon248@gmail.com>
2013-06-28 10:29:36 -0400

    Created a MetaBax class as opposed to namespace, and started to port over some of the demonstration code into functions in an abstractable manner (examples are provided that will work with SDL's 32-bit ARGB format).

--- libmetabax/MetaBax.cpp
+++ libmetabax/MetaBax.cpp
@@ -14,42 +14,106 @@
  * COPYRIGHT file, or visit http://github.com/Breakthrough/MetaBax.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+
 #include <cassert>
+#include "MetaBax.h"
 
-namespace MetaBax
+
+MetaBax::MetaBax(int sig_line_len, int sig_num_lines);
 {
-///
+    _composite = new float[height * width * 4];
+
+}
 
 
-void avs_row_to_img(float* sig_row, int sig_row_len,
-                    void*  img_row, int img_row_len, fptr_frgb2img frgb_row_to_img_row)
+void  MetaBax::Update();     // Updates signal with image including RX noise, etc.
+{
+    // All this has to do is update the composite signal.
+}
+
+
+void MetaBax::avs_row_to_img(float* sig_row, int sig_row_len,
+    void*  img_row, int img_w, fptr_frgb2img frgb_row_to_img_row)
 {
     // sig_row_len = samples/row
-    // img_row_len = pixels/row
+    // img_w = pixels/row
 
-    float *frgb_row = new float[3 * img_row_len];
+    float *frgb_row = new float[3 * img_w];
     float *frgb_val = frgb_row;
 
-    for (int i = 0; i < img_row_len; i++)
+    for (int i = 0; i < img_w; i++)
     {
         // Decode signal into R, G, and B components.
-        float r, g, b;
+        float r = 0.0f,
+              g = 0.0f,
+              b = 0.0f;
 
-        //...
+        //
+        // TODO: Actually decode signal values above.
+        //
 
         *(frgb_val++) = r;
         *(frgb_val++) = g;
         *(frgb_val++) = b;
     }
 
-    frgb_row_to_img_row(frgb_row, img_row, img_row_len);
+    frgb_row_to_img_row(frgb_row, img_row, img_w);
         
 
     delete[] frgb_row;
 }
 
 
-void frgb_row_conv_32bppARGB(float *frgb_row, void* img_row, int img_row_len)
+void MetaBax::LoadImage(int img_w, int img_h, char* img_ptr, int stride,
+    fptr_img_row_to_yiq img_row_to_yiq)
+{
+    for (int sig_row = 0; sig_row < sig_num_rows; sig_row++)
+    {
+        char* img_row_ptr = img_ptr;
+        
+        img_row_ptr += (((img_h * sig_row) / sig_num_rows) * img_w * stride);
+
+        // Convert image row to YIQ (stored in private class vars).
+        img_row_to_yiq(img_row_ptr, img_w, stride, sig_row);
+
+        // Premultiply I/Q by sin/cos before further filters?
+        // Maybe, maybe not - some DSP encoders do NOT.
+
+        //
+        // TODO: Investigate above.
+        //
+    }
+}
+
+
+static void MetaBax::ARGB32_row_to_yiq(char* img_row_ptr, int img_w,
+    int stride, int sig_row, float* y_buffer, float* i_buffer, float* q_buffer)
+{
+    // nearest neighbour for the row values.
+    int yiq_offset = sig_row * sig_row_len;
+    for (int i = 0; i < sig_row_len; i++)
+    {
+        char* pixel = img_row_ptr;
+        pixel += (((img_w * i) / sig_row_len) * stride);
+
+        // pixel[0] == alpha channel == unused
+        y_buffer[yiq_offset + i] = 
+            0.2990f * pixel[1] + 0.5870f * pixel[2] + 0.1140f * pixel[3];
+        i_buffer[yiq_offset + i] = 
+            0.5957f * pixel[1] - 0.2745f * pixel[2] - 0.3214f * pixel[3];
+        q_buffer[yiq_offset + i] = 
+            0.2115f * pixel[1] - 0.5226f * pixel[2] + 0.3111f * pixel[3];
+
+        //
+        // TOOD: Add source for conversion matrix used above.
+        //
+
+    }
+}
+
+
+static void MetaBax::FRGB_row_to_ARGB32(float *frgb_row, char* img_row,
+    int img_w, int stride)
 {
     int   i         = 0;
     float *frgb_val = frgb_row;     
@@ -58,15 +122,11 @@ void frgb_row_conv_32bppARGB(float *frgb_row, void* img_row, int img_row_len)
     frgb_row--;
     img_row--;
     // Loop through and set each pixel value in the row (32bpp RGBA).
-    while (i < img_row_len)
+    while (i < img_w)
     {
         *(++img_val) =   (int)(*(++frgb_val)) << 16
                        | (int)(*(++frgb_val)) << 8
                        | (int)(*(++frgb_val));
         i++;
     }
-}
-
-
-///
 }
--- libmetabax/MetaBax.h
+++ libmetabax/MetaBax.h
@@ -14,25 +14,54 @@
  * COPYRIGHT file, or visit http://github.com/Breakthrough/MetaBax.          *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-namespace MetaBax
-{
-///
 
-typedef void (*fptr_frgbrow2img)(float*, void*, int);
+class MetaBax {
+  public:
 
-// Signal Encoding:
+    // Function pointer to floating-point RGB row blitter (converts floating point
+    // values decoded from the analog video signal into an integer surface format).
+    typedef void (*fptr_blit_row)(float*, char*, int, int);
+    //                              |       |     |
+    //                              |       |     \---- number of pixels in row
+    //                              |       \---- generic pointer to surface data
+    //                              \---- pointer floating point values RGBRGB-packed
 
-// Signal Decoding:
-template<class T>
-void avs_row_to_img(float* sig_row, int sig_row_len,
-                    void*  img_row, int img_row_len, fptr_frgbrow2img frgb2img);
-
-// Pixel Conversion Formats (may be moved to separate file):
-void frgb_to_SDL_32bpp(float *frgb_row, void* img_row, int img_row_len);
+    typedef void (*fptr_img_row_to_yiq)(char*, int, int, int, float*, float*, float*);
 
 
-///
-}
+    MetaBax(int sig_line_len = 755, int sig_num_lines = 480);
 
-// namespace MB = MetaBax;
+    void  Update();     // Updates signal with image including RX noise, etc.
+    void LoadImage(int img_w, int img_h, char* img_ptr, int stride,
+        fptr_img_row_to_yiq img_row_to_yiq);
 
+
+    static void ARGB32_row_to_yiq(char*  img_row_ptr, int    img_w,
+                                  int    stride,      int    sig_row,
+                                  float* y_buffer,    float* i_buffer, float* q_buffer);
+
+    static void FRGB_row_to_ARGB32(float* frgb_row, char* img_row,
+                                   int    img_w,    int   stride);
+
+
+    int   h_sync;       // [0, sig_row_len)  then wraps around
+    int   v_sync;       // [0, sig_num_rows) then wraps around
+
+
+    float h_freq_err;   // [0.0f, 1.0f] or [0.0f, Infty)?
+    float v_freq_err;   // [0.0f, 1.0f]
+    float rx_sig_level; // [0.0f, 1.0f]
+
+    float *_composite;
+
+    float *_y_buffer;   // len is equal to sig_row_len * sig_num_rows.......
+    float *_i_buffer;   // Consumes as much memory as the composite signal.
+    float *_q_buffer;
+
+
+
+  private:
+    int   sig_num_rows;
+    int   sig_row_len;
+
+};
